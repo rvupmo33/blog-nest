f5a5b4fdd98fe93952c1c440ba016def2c9d56e1
davidfasanu <fasanu.david@gmail.com>
2024-11-11 21:55:27 -0500

    login implemented.

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const usersControllers = require("../controllers/users-controller");
 const router = express.Router();
 
+router.get('/', usersControllers.home);
+
 router.post("/signup", usersControllers.createUser);
+
+router.post("/login", usersControllers.loginUser);
 
 module.exports = router;
