1d44b4abf15306eac36f00180a24ab31a850be58
Niks-stack <nbijju@my.centennialcollege.ca>
2024-11-21 18:12:33 -0500

    Adding hamburger button and .css files

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -3,11 +3,19 @@ import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import { Image } from "lucide-react";
+import "./MainNavigation.css";
 
 const MainNavigation = () => {
   return (
     <div>
       <MainHeader>
+        {/* Hamburger button that appears only on mobile */}
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+
         {/* <PanelRightClose /> */}
         <h1 className="main-navigation__title">
           <Link to="/">
@@ -17,6 +25,7 @@ const MainNavigation = () => {
             </div>
           </Link>
         </h1>
+        
         <nav className="main-navigation__header-nav">
           <NavLinks />
         </nav>
