c693232563ac4602762776ed48f682aa72e48277
Rubiya97 <rubiyasultana97@gmail.com>
2024-11-28 23:23:36 -0500

    UpdateBlog

--- frontend/src/blogs/pages/UpdateBlog.js
+++ frontend/src/blogs/pages/UpdateBlog.js
@@ -35,25 +35,29 @@ const UpdateBlog = ({ blogs, onUpdateBlog }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Title</label>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Content</label>
-        <textarea
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        />
-      </div>
-      <button type="submit">Update Blog</button>
-    </form>
+    <div className="create-blog-form">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Content</label>
+          <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+        <button type="submit">Update Blog</button>
+      </form>
+    </div>
   );
 };
 
 export default UpdateBlog;
+
+
